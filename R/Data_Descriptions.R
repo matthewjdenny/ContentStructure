0c8fc7d78a7f81c8b6b19cec77f0395eab04d63b
Matt Denny <matthewjdenny@gmail.com>
2015-05-24 20:50:18 -0400

    Removed names and updated documentation

--- R/Data_Descriptions.R
+++ R/Data_Descriptions.R
@@ -51,14 +51,15 @@ NULL
 #' and are as follows:
 #'   
 #' \itemize{ 
-#'   \item document_word_matrix -- a number of emails (121) by number of unique terms (221) matrix the records the number of times each term was used in each email.
+#'   \item document_word_matrix -- a number of emails (121) by number of unique terms (204) matrix the records the number of times each term was used in each email. Note that stopwords 
+#'   have been removed as have names.
 #' }
 #'
 #' @docType data
 #' @keywords datasets
 #' @name document_word_matrix
 #' @usage data(document_word_matrix)
-#' @format A a number of emails (121) by number of unique terms (221) dataframe that records the number of times each term was used in each email.
+#' @format A a number of emails (121) by number of unique terms (204) dataframe that records the number of times each term was used in each email.
 NULL
 
 #' Department Manager Email dataset -- Vocabulary
@@ -71,12 +72,13 @@ NULL
 #' and are as follows:
 #'   
 #' \itemize{
-#'   \item a vector containing all unique terms.
+#'   \item a vector containing all 204 unique terms. Note that stopwords 
+#'   have been removed as have names.
 #' }
 #'
 #' @docType data
 #' @keywords datasets
 #' @name vocabulary
 #' @usage data(vocabulary)
-#' @format A dataframe with 20 rows and 2 columns.
+#' @format A dataframe with 204 rows and one word per row.
 NULL
