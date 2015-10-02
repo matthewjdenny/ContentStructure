f8c3ef958f48a20b904aee81766b6ce4bc9bc4c5
Matt Denny <matthewjdenny@gmail.com>
2015-10-02 10:56:33 -0400

    change output directory names

--- R/Run_Full_Model.R
+++ R/Run_Full_Model.R
@@ -17,8 +17,8 @@
 #' @param mixing_variable if not NULL, specifies the name of the binary variable in the author_attributes dataset that will be used to estimate mixing parameter effects.
 #' @param Seed Sets the seed in R and C++ for replicability.
 #' @param save_results_to_file A logical value indicating whether intermediate results should be saved to file or whether they will be return to the R session.
-#' @param data_directory The directory where our data_name file is stored. This is also where all output will be saved. Defaults to NULL if save_results_to_file == FALSE.
-#' @param data_name The name of the .Rdata file you would like to save model output in. Defaults to NULL if save_results_to_file == FALSE.
+#' @param output_directory This is where all output will be saved. Defaults to NULL if save_results_to_file == FALSE.
+#' @param output_filename The name of the .Rdata file you would like to save model output in. Defaults to NULL if save_results_to_file == FALSE.
 #' @param Main_Estimation_Results A list object returned by previous model estimation to be supplied if the user wishes to select run_MH_only == TRUE. Useful if the user would like to specify a greater number of iterations for the final step of LSM estimation.
 #' @return Does not return anything, just saves everything to our data_directory folder.
 #' @export
@@ -37,8 +37,8 @@ Run_Full_Model <- function(Auth_Attr,
                            mixing_variable = NULL,
                            Seed = 123456,
                            save_results_to_file = FALSE,
-                           data_directory = NULL,
-                           data_name = NULL,
+                           output_directory = NULL,
+                           output_filename = NULL,
                            Main_Estimation_Results = NULL
                            ){
     
@@ -47,13 +47,13 @@ Run_Full_Model <- function(Auth_Attr,
         substr(x, nchar(x)-n+1, nchar(x))
       }
       
-      lastchar <- substrRight(data_directory,1)
+      lastchar <- substrRight(output_directory,1)
       
       if(lastchar == "/"){
         #we are all set
       }else{
         #add a trailing slash to we save to right place
-        data_directory <- paste(data_directory,"/",sep = "")
+        output_directory <- paste(output_directory,"/",sep = "")
       }
     }
     
@@ -64,37 +64,33 @@ Run_Full_Model <- function(Auth_Attr,
     }
     
     if(!run_MH_only){
-        Results <- Run_Inference(Number_Of_Iterations = main_iterations, 
-                                 Base_Alpha =0.1, 
-                                 Base_Beta = 0.01, 
-                                 Number_Of_Topics = topics, 
-                                 Topic_Step_Itterations = 1, 
-                                 Sample_Step_Itterations = 1000, 
-                                 Proposal_Variance = 0.5, 
-                                 seed = Seed, 
-                                 Number_of_Clusters = clusters,
-                                 Itterations_Before_Cluster_Assingment_Updates = 2,
-                                 Adaptive_Metropolis_Target_Accept_Rate = 0.2,
-                                 slice_sample_alpha_step_size = 1,
-                                 MH_prior_standard_deviation = 5,
-                                 Slice_Sample_Alpha = F,
-                                 Number_of_Binary_Mixing_Parameters = num_bin_mix_vars, 
-                                 Mixing_Variable = mixing_variable,
-                                 Author_Attributes= Auth_Attr, 
-                                 Document_Edge_Matrix = Doc_Edge_Matrix,
-                                 Document_Word_Matrix = Doc_Word_Matrix, 
-                                 Vocabulary = Vocab,
-                                 output_folder_path = data_directory,
-                                 output_file = data_name,
-                                 Latent_Dimensions = latent_space_dimensions,
-                                 save_results_to_file = save_results_to_file)
+        Results <- Run_Inference(
+         Number_Of_Iterations = main_iterations, 
+         Base_Alpha =0.1, 
+         Base_Beta = 0.01, 
+         Number_Of_Topics = topics, 
+         Topic_Step_Itterations = 1, 
+         Sample_Step_Itterations = 1000, 
+         Proposal_Variance = 0.5, 
+         seed = Seed, 
+         Number_of_Clusters = clusters,
+         Itterations_Before_Cluster_Assingment_Updates = 2,
+         Adaptive_Metropolis_Target_Accept_Rate = 0.2,
+         slice_sample_alpha_step_size = 1,
+         MH_prior_standard_deviation = 5,
+         Slice_Sample_Alpha = F,
+         Number_of_Binary_Mixing_Parameters = num_bin_mix_vars, 
+         Mixing_Variable = mixing_variable,
+         Author_Attributes= Auth_Attr, 
+         Document_Edge_Matrix = Doc_Edge_Matrix,
+         Document_Word_Matrix = Doc_Word_Matrix, 
+         Vocabulary = Vocab,
+         Latent_Dimensions = latent_space_dimensions)
     }else{
       Results <- Main_Estimation_Results
     }
       
     Results <-  Run_MH_To_Convergence(
-                  input_file = paste("Model_Output_",data_name,sep = ""),
-                  output_file = paste("Sample_",data_name,sep = ""),
                   sample_step_burnin = sample_step_burnin,
                   itterations = sample_step_iterations,
                   sample_every = sample_step_sample_every, 
@@ -103,16 +99,21 @@ Run_Full_Model <- function(Auth_Attr,
                   adaptive_metropolis_update_every = 1000, 
                   use_adaptive_metropolis = 1, 
                   MH_prior_standard_deviation = 5 ,
-                  data_dir = data_directory ,
                   seed = Seed,
-                  save_results_to_file = save_results_to_file,
                   Main_Estimation_Results = Results)
-
+    
     # add in raw input information so that we can access it later
     Results$author_attributes = Auth_Attr
     Results$document_edge_matrix = Doc_Edge_Matrix
     Results$document_word_matrix = Doc_Word_Matrix
     Results$vocabulary = Vocab
+    Results$mixing_variable = mixing_variable
+    Results$latent_space_dimensions = latent_space_dimensions
+    Results$num_topics = topics
+    Results$num_actors = nrow(Auth_Attr)
     
+    if(save_results_to_file){
+      save(Results, file = paste(output_directory,output_filename,".Rdata",sep = ""))
+    }
     return(Results)
 }
